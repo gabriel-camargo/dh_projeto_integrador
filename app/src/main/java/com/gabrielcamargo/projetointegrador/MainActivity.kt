21f9e23e33687b7944af0a6c70d8576a3d141653
Mailson <mayllsonribeiro6@gmail.com>
2020-11-21 12:11:21 -0300

    Ajuste activity

--- app/src/main/java/com/gabrielcamargo/projetointegrador/MainActivity.kt
+++ app/src/main/java/com/gabrielcamargo/projetointegrador/MainActivity.kt
@@ -9,9 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment, BuscaFragment())
-                .commit()
+        
     }
 }
