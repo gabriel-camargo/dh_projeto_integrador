ec37cffc965234ef1794ae28b2c9ebc4c40fa949
Gabriel R. Camargo <gabriel.rodrigocam@gmail.com>
2020-11-22 13:05:56 -0300

    lista de filmes com imagens

--- app/src/main/java/com/gabrielcamargo/projetointegrador/favoritemovies/movielist/model/MovieListModel.kt
+++ app/src/main/java/com/gabrielcamargo/projetointegrador/favoritemovies/movielist/model/MovieListModel.kt
@@ -2,5 +2,6 @@ package com.gabrielcamargo.projetointegrador.favoritemovies.movielist.model
 
 data class MovieListModel(
     val nome: String,
-    val qtd: Int
+    val qtd: Int,
+    val img: Int
 )
