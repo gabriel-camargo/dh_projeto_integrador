352269c82c6a15a5c122e918e3abbdf6f48a9a3f
Gabriel R. Camargo <gabriel.rodrigocam@gmail.com>
2021-01-30 13:42:07 -0300

    ajusta login do facebook para botão convencional - salva nome no login com email e senha

--- app/src/main/java/com/cgmdigitalhouse/cinelist/SignUpActivity.kt
+++ app/src/main/java/com/cgmdigitalhouse/cinelist/SignUpActivity.kt
@@ -5,55 +5,87 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.core.content.res.ResourcesCompat
-import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var _auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        _auth = Firebase.auth
 
         // Fonte DK
         val fontDK = ResourcesCompat.getFont(this, R.font.dk_butterfly_ball)
         val textLogo: TextView = findViewById(R.id.txt_logo)
         textLogo.typeface = fontDK
 
-        // Variaveis
         val btnSignUp = findViewById<Button>(R.id.btn_signUp)
-
         btnSignUp.setOnClickListener {
+            val edtName = findViewById<TextInputLayout>(R.id.edt_singupNomeLayout)
             val edtLoginEmail = findViewById<TextInputLayout>(R.id.edt_singupEmailLayout)
             val edtPassword = findViewById<TextInputLayout>(R.id.edt_singupPasswordLayout)
-            val edtConfirmPassword = findViewById<TextInputLayout>(R.id.edt_singupPasswordConfirmacaoLayout)
+            val edtConfirmPassword =
+                findViewById<TextInputLayout>(R.id.edt_singupPasswordConfirmacaoLayout)
 
+            edtName.error = null
+            edtLoginEmail.error = null
+            edtPassword.error = null
+            edtConfirmPassword.error = null
+
+            val name = edtName.editText!!.text.toString().trim()
             val email = edtLoginEmail.editText!!.text.toString().trim()
             val password = edtPassword.editText!!.text.toString().trim()
             val passwordConfirm = edtConfirmPassword.editText!!.text.toString().trim()
 
             when {
+                name.isEmpty() -> {
+                    edtName.error = "Preencha o campo de nome!"
+                }
                 email.isEmpty() -> {
-                    Toast.makeText(this, "Preencha o campo de email!", Toast.LENGTH_SHORT).show()
+                    edtLoginEmail.error = "Preencha o campo de e-mail!"
                 }
                 password.isEmpty() -> {
-                    Toast.makeText(this, "Preencha o campo de senha!", Toast.LENGTH_SHORT).show()
+                    edtPassword.error = "Preencha o campo de senha!"
                 }
                 password != passwordConfirm -> {
-                    Toast.makeText(this, "As senhas fornecidas são diferentes!", Toast.LENGTH_SHORT).show()
+                    edtConfirmPassword.error = "As senhas fornecidas não correspondem!"
                 }
                 else -> {
-                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener{
-                            if(it.isSuccessful) {
-                                Toast.makeText(this, "Usuário cadastrado com sucesso!", Toast.LENGTH_SHORT).show()
+                    _auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                val profileUpdates = UserProfileChangeRequest.Builder()
+                                    .setDisplayName(name).build()
 
-                                val intent = Intent(this, LoginActivity::class.java)
+                                _auth.currentUser!!.updateProfile(profileUpdates)
+
+                                Snackbar.make(
+                                    findViewById(android.R.id.content),
+                                    "Usuário cadastrado com sucesso!",
+                                    Snackbar.LENGTH_SHORT
+                                ).show()
+
+                                val intent = Intent(this, MainActivity::class.java)
+                                intent.apply {
+                                    putExtra("provider", ProviderType.BASIC)
+                                }
                                 startActivity(intent)
                                 finish()
+
                             } else {
-                                Toast.makeText(this, "Erro ao cadastrar usuário", Toast.LENGTH_SHORT).show()
+                                Snackbar.make(
+                                    findViewById(android.R.id.content),
+                                    "Erro ao cadastrar usuário!",
+                                    Snackbar.LENGTH_SHORT
+                                ).show()
                             }
                         }
                 }
