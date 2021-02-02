b3f5e19e870737403d0c33c4c8fc3dff4c6ab55a
Gabriel Camargo <gabriel.rodrigocam@gmail.com>
2021-02-01 23:41:43 -0300

    fix-details-sprint-3: Foto padrão para o autor

--- app/src/main/java/com/cgmdigitalhouse/cinelist/moviedetails/cast/model/CastModel.kt
+++ app/src/main/java/com/cgmdigitalhouse/cinelist/moviedetails/cast/model/CastModel.kt
@@ -6,7 +6,7 @@ import com.google.gson.annotations.SerializedName
 data class CastModel (
     @SerializedName("name") val name: String,
     @SerializedName("character") val character: String,
-    @SerializedName("profile_path") val profilePath: String
+    @SerializedName("profile_path") val profilePath: String?
 ){
     fun getPathImage():String{
         return NetworkUtils.BASE_URL_IMAGE+profilePath
